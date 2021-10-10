d2125d9820a81adff2e1d22caa5e06e0d10025b3
Ehtisham Ul Haq <46608858+ehtishamdot@users.noreply.github.com>
2021-10-10 11:58:36 +0500

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,1 @@
-const data = async () => {
-  const res = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api-mobilespecs.azharimm.site/v2/latest`
-  );
-  const data = await res.json();
-  console.log(data);
-};
 
-data();
